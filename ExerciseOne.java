3263208a0889f91ef541c4b2d76e6247ff06de5a
Angeline Lee <angeline.lee4954@gmail.com>
2020-09-10 20:23:55 -0700

    minor changes to dog and added Hello Coder !!! message

--- ExerciseOne.java
+++ ExerciseOne.java
@@ -1,12 +1,12 @@
 class ExerciseOne {
 
     public static void printAscii() {
-        System.out.println("                        (\\");
-        System.out.println("                       (\\_\\_^__o");
-        System.out.println("                ___     `-'/ `_/");
-        System.out.println("               '`--\\______/ |");
-        System.out.println("          '        /         |");
-        System.out.println("      `    .  ' `-`/.------'\\^-'");
+        System.out.println("                        (\\            - - - -");
+        System.out.println("                       (\\_\\_^__o    /  Hello  \\");
+        System.out.println("                ___     `-'/ `_/   <   Coder   |");
+        System.out.println("               '`--\\______/ |       \\   !!!   /");
+        System.out.println("          '        /        |         - - - -");
+        System.out.println("     `    .  ' `-`/.------'\\^-'");
     }
 
     public static void main(String[] args) {
