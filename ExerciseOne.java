756a79d1b871013546b17884635cea991b0cb2b9
Angeline Lee <angeline.lee4954@gmail.com>
2020-09-10 19:51:04 -0700

    added ascii dog

---
+++ ExerciseOne.java
@@ -0,0 +1,17 @@
+class ExerciseOne {
+
+    public static void printAscii() {
+        System.out.println("                        (\\");
+        System.out.println("                       (\\_\\_^__o");
+        System.out.println("                ___     `-'/ `_/");
+        System.out.println("               '`--\\______/ |");
+        System.out.println("          '        /         |");
+        System.out.println("      `    .  ' `-`/.------'\\^-'");
+    }
+
+    public static void main(String[] args) {
+        printAscii();
+    }
+
+}
+
